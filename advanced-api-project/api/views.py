0515531b3b60eef6626c32db33a6ae517c2b6f42
Moses Musoke <123477434+MosesMusoke@users.noreply.github.com>
2024-09-16 03:24:02 +0000

    changes3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from django.http import HttpResponse
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters.SearchFilter, filters.OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
 from .models import Book
@@ -11,7 +11,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 class ListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']  # Filtering on title, author's name, and publication year
     search_fields = ['title', 'author__name']  # Searching by title and author's name
     ordering_fields = ['title', 'publication_year']  # Ordering by title and publication year
